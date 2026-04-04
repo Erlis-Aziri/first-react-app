6fee1d80b8af67dc328cc629b712ec106f84a43d
Erlis-Aziri <erlis.aziri2010@gmail.com>
2026-04-04 13:26:50 +0200

    dd

--- screens/ExcerciseScreen.js
+++ screens/ExcerciseScreen.js
@@ -1,16 +1,26 @@
 import { NavigationContainer } from 'react';
-import {    Text, Stylesheet, view } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 const ExcerciseScreen = () => {
     let message = "hello world";
 
     return (
 
         <View>
-        <Text>
-            {message}
-        </Text>
+        <Text style={styles.textstyle1}> {message}</Text>
+        <Text style={styles.textstyle2}> {message}</Text>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    textstyle1: {
+        color: '#00ff6a',
+        fontSize: 30
+    },
+    textstyle2: {
+        fontSize: 30,
+        color: '#ff0000'
+
+    }
+});
 
 export default ExcerciseScreen;
