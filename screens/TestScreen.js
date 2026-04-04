89ea9c2db856a789b61808bac4967ecabc020a10
Erlis-Aziri <erlis.aziri2010@gmail.com>
2026-04-04 11:46:53 +0200

    cc

---
+++ screens/TestScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const TestScreen = () => {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.textStyle}>This is Test screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ff0000',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textStyle: {
+        fontSize: 30
+    }
+});
+
+export default TestScreen;
+
