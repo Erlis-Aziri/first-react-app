e49eba455aa7bfa0f525f876cb3fd232ca291ff1
Erlis-Aziri <erlis.aziri2010@gmail.com>
2026-04-25 10:54:48 +0200

    sss

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MainScreen from './screens/MainScreen';
-import TestScreen from './screens/TestScreen';
-import ExcerciseScreen from './screens/ExcerciseScreen';
 
+import MainScreen from './screens/MainScreen';
+import ExerciseScreen from './screens/ExerciseScreen';
+import ListScreen from './screens/ListScreen';
+import CarsList from './screens/Test';
+import Studentlist from './screens/StudentScreen'; 
+import ButtonScreen from './screens/ButtonScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ExcerciseScreen}></Stack.Screen>
+      <StatusBar style="auto" />
+
+      <Stack.Navigator
+        initialRouteName="Button"
+        screenOptions={{
+          title: "Students"
+        }}
+      >
+        <Stack.Screen name="Home" component={MainScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
+        <Stack.Screen name="Exercise" component={ExerciseScreen} />
+        <Stack.Screen name="Test" component={CarsList} />
+        <Stack.Screen name="Button" component={ButtonScreen}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
